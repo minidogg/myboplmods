28d9c87717447fe3fb178758b633f1482f35ea62
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2023-12-22 10:58:01 -0600

    Add files via upload

---
+++ abilitystorm/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using System;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System.Reflection;
+using BoplFixedMath;
+using UnityEngine;
+
+namespace BoplBattleTemplate
+{
+    [BepInPlugin(pluginGuid, "Ability Storm", "1.0.0")]
+    [BepInProcess("BoplBattle.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.unluckycrafter.abilitystorm";
+        public static IInputSystem Current { get; }
+
+        private void Awake()
+        {
+            
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");//feel free to remove this
+            Harmony harmony = new Harmony(pluginGuid);
+            MethodInfo original = AccessTools.Method(typeof(AbilitySpawner), "UpdateSim");
+            MethodInfo patch = AccessTools.Method(typeof(Plugin), "UpdateSim_p");
+            harmony.Patch(original, new HarmonyMethod(patch));
+
+
+            //MethodInfo original = AccessTools.Field(typeof());
+        }
+        public static bool UpdateSim_p(Fix simDeltaTime, ref Fix ___time, ref Fix ___SpawnDelay, ref int ___spawns, ref int ___MaxSpawns, ref AbilitySpawner __instance, ref FixTransform ___fixTrans)
+        {
+            ___time += (GameTime.IsTimeStopped() ? Fix.Zero : simDeltaTime);
+            if (___time > 1L)
+            {
+                ___time = Fix.Zero;
+                //Spawn()
+                DynamicAbilityPickup dynamicAbilityPickup = FixTransform.InstantiateFixed<DynamicAbilityPickup>(__instance.pickupPrefab, ___fixTrans.position);
+                dynamicAbilityPickup.InitPickup(null, null, Updater.RandomUnitVector());
+                dynamicAbilityPickup.SwapToRandomAbility();
+                //
+                ___spawns++;
+                if (___spawns >= ___MaxSpawns)
+                {
+                    //__instance.enabled = false;
+                }
+            }
+            return false;
+        }
+
+
+
+
+        /*        public static bool UpdateSim_p(Fix simDeltaTime, ref Fix ___time, ref Fix ___SpawnDelay, ref int ___spawns, ref int ___MaxSpawns,ref AbilitySpawner __instance, ref FixTransform ___fixTrans)
+                {
+                    ___time += (GameTime.IsTimeStopped() ? Fix.Zero : simDeltaTime);
+                    if (___time > ___SpawnDelay)
+                    {
+                        ___time = Fix.Zero;
+                        //spawn
+                        DynamicAbilityPickup dynamicAbilityPickup = FixTransform.InstantiateFixed<DynamicAbilityPickup>(__instance.pickupPrefab, ___fixTrans.position);
+                        dynamicAbilityPickup.InitPickup(null, null, Updater.RandomUnitVector());
+                        dynamicAbilityPickup.SwapToRandomAbility();
+                        //
+                        ___spawns++;
+                        if (___spawns >= ___MaxSpawns)
+                        {
+                            __instance.enabled = false;
+                        }
+                    }
+                    return false;
+                }*/
+
+    }
+    }
